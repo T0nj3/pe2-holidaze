28df916d91eea3e2604a0083d411d416ab3edd18
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-11-06 19:27:31 +0100

    removed test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
-import Header from "./components/Header"
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import "./App.css";
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="min-h-screen bg-base text-text flex flex-col items-center justify-center">
+    <div className="min-h-screen bg-[#292929] text-white flex flex-col"> 
       <Header />
-      <h1 className="text-5xl font-serif mb-4">Holidaze 🌴</h1>
-      <p className="text-lg text-white/70 mb-6">Tailwind is working perfectly!</p>
-
-      <button
-        onClick={() => setCount(count + 1)}
-        className="bg-olive px-6 py-3 rounded-md hover:bg-olive/80 transition"
-      >
-        Clicks: {count}
-      </button>
     </div>
-  )
+  );
 }
