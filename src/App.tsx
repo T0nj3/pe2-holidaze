f9f39dcccf3ff70f3ac9dd51d2f9a06d70928fef
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-11-06 19:13:09 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
+import Header from "./components/Header"
 import './App.css'
-import Header from "./components/Header";
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="min-h-screen bg-base text-text">
+    <div className="min-h-screen bg-base text-text flex flex-col items-center justify-center">
       <Header />
-      <div className="pt-32 flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-serif mb-4">Holidaze 🌴</h1>
-        <p className="text-lg text-white/70 mb-6">
-          Tailwind is working perfectly!
-        </p>
-        <button className="bg-olive px-6 py-3 rounded-md hover:bg-olive/80 transition">
-          Book your next stay
-        </button>
-      </div>
+      <h1 className="text-5xl font-serif mb-4">Holidaze 🌴</h1>
+      <p className="text-lg text-white/70 mb-6">Tailwind is working perfectly!</p>
+
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-olive px-6 py-3 rounded-md hover:bg-olive/80 transition"
+      >
+        Clicks: {count}
+      </button>
     </div>
-  );
+  )
 }
