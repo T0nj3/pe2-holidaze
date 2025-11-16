3bc1c073e9748be222aa2679f440f2c587b3101c
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-11-16 19:26:07 +0100

    correction with netlify issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import "./App.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="min-h-screen bg-[#292929] text-white flex flex-col relative overflow-hidden">
