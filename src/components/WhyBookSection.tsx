10a38a42176fbe01eab6a7340380723f61668d88
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-12-04 19:47:24 +0100

    added cursor pointer to whybooksection

--- src/components/WhyBookSection.tsx
+++ src/components/WhyBookSection.tsx
@@ -40,7 +40,7 @@ export default function WhyBookSection() {
           Why book with Holidaze?
         </h2>
 
-        <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 cursor-pointer">
           {features.map(({ title, description, Icon }) => (
             <div
               key={title}
